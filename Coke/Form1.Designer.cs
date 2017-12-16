f7eb9391332fb3817dd6562dd92540209187cbf0
masarjoe9 <masarjoe9@gmail.com>
2017-12-16 15:59:43 -0500

    Project DUMP
    This is the project folder from VS.

---
+++ Coke/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Coke
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.noButton = new System.Windows.Forms.Button();
+            this.yesButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Coke.Properties.Resources.coke;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(225, 184);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(393, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hello! Would you like a coke?";
+            // 
+            // noButton
+            // 
+            this.noButton.Location = new System.Drawing.Point(561, 171);
+            this.noButton.Name = "noButton";
+            this.noButton.Size = new System.Drawing.Size(75, 23);
+            this.noButton.TabIndex = 2;
+            this.noButton.Text = "No";
+            this.noButton.UseVisualStyleBackColor = true;
+            this.noButton.Click += new System.EventHandler(this.noButton_Click);
+            // 
+            // yesButton
+            // 
+            this.yesButton.Location = new System.Drawing.Point(480, 172);
+            this.yesButton.Name = "yesButton";
+            this.yesButton.Size = new System.Drawing.Size(75, 23);
+            this.yesButton.TabIndex = 3;
+            this.yesButton.Text = "Yes";
+            this.yesButton.UseVisualStyleBackColor = true;
+            this.yesButton.Click += new System.EventHandler(this.yesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 206);
+            this.Controls.Add(this.yesButton);
+            this.Controls.Add(this.noButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Coke.exe";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button noButton;
+        private System.Windows.Forms.Button yesButton;
+    }
+}
+
---
+++ Coke/Form1.cs
@@ -0,0 +1,79 @@
+﻿//Imports
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Coke
+{
+    //Main Form
+    public partial class Form1 : Form
+    {
+        //Create the form and then center it
+        public Form1()
+        {
+            InitializeComponent();
+            CenterToScreen();
+        }
+
+        //No Button Click
+        private void noButton_Click(object sender, EventArgs e)
+        {
+            //If they dont want to play just close the app
+            Close();
+        }
+
+        //Import opening of CD Dlls
+        [DllImport("winmm.dll", EntryPoint = "mciSendString")]
+        public static extern int mciSendStringA(string lpstrCommand, string lpstrReturnString, int uReturnLength, int hwndCallback);
+
+        //Yes button Click
+        private void yesButton_Click(object sender, EventArgs e)
+        {
+            Boolean hasCD = false;
+
+            //Get Drive Types
+            DriveInfo[] dr = System.IO.DriveInfo.GetDrives();
+
+            //Find which drive is the CD Drive
+            //lets hope they dont have two or they will both open :) extra support for that awesome coke they will enjoy??
+            foreach (DriveInfo d in dr)
+            {
+                //Look for CDdrive
+                if (d.DriveType == DriveType.CDRom)
+                {
+                    //Tell me you do have a CD
+                    hasCD = true;
+
+                    //Set and open the CD drive
+                    mciSendStringA("open " + d.Name + ": type CDaudio alias drive" + d.Name , "", 0, 0);
+                    mciSendStringA("set drive" + d.Name + " door open", "", 0, 0);
+                }
+            }
+            //Show a message depending drives installed
+            if (hasCD)
+            {
+                //Show message for having a CD drive
+                string message = "Here is a cup holder";
+                string caption = "Hey!";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show(message, caption, buttons);
+            }
+            else
+            {
+                //Show message for not having a CD drive
+                string message = "Your computer does not have a CDRom Drive for me to open, but if it did it would be open right now";
+                string caption = "Hey!";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                MessageBox.Show(message, caption, buttons);
+            }
+        }
+    }
+}
